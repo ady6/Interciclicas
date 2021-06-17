9c3de5c03475f5dcadfe05c714dd69607ae7bb1a
ady6 <82877241+ady6@users.noreply.github.com>
2021-06-17 11:25:30 +0200

    Archivos

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function asperger(){
+    document.getElementById("asperger").innerHTML= "<img src='imagen/asperger1.jpg'> </img> <img src='imagen/asperger2.jpg'> </img>";
+    
+  }
+
+  function deficit(){
+    document.getElementById("deficit").innerHTML= "<img src='imagen/deficit1.jpg'> </img> <img src='imagen/deficit2.jpg'> </img>";
+    }
